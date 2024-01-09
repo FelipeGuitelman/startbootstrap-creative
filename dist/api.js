3ce11e46f773801246f6e8aebef3a88a08ea7a37
Felipe Guitelman <felipe.guitelman@gmail.com>
2024-01-09 12:20:34 -0300

    feed ig

---
+++ dist/api.js
@@ -0,0 +1,44 @@
+'use strict'
+
+require('dotenv').config()
+
+const galery = document.querySelector('.galery')
+const feed = document.querySelector('.contenedor-galery')
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+
+const token = process.env.IG_TOKEN
+
+const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=20&access_token= ${token}`
+
+fetch(url)
+.then(res => res.json)
+.then(data => CrearHtml(data.data))
+
+function CrearHtml(data){
+  for (const img of data) {
+    galery.innerHTML += `
+    <div class="image-overflow">
+      <img loading="lazy" src="${img.media_url}" alt="">
+      <div class="opacity-hover">
+        <a href="${img.permalink}" class="caption">
+          <p>
+            ${img.caption.slice(0 , 100)}
+          </p>
+        </a>
+      </div>
+    </div>
+    `;
+  }
+}
+
+next.addEventListener('clcick', moveGalery);
+prev.addEventListener('clcick', moveGalery);
+
+function moveGalery (e){
+  if (e.targe.id === 'next' || e.target.parentElement.id === 'next') {
+    feed.scrollLeft += feed.offsetWidth;
+  } else {
+    feed.scrollLeft -= feed.offsetWidth;
+  }
+}
